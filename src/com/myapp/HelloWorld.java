04a8076c676df9a03d96c62c3cc96c9b8d9ef412
devopschn <76941371+devopschn@users.noreply.github.com>
2021-03-24 11:16:13 +0530

    Update HelloWorld.java
    2nd commit

--- src/com/myapp/HelloWorld.java
+++ src/com/myapp/HelloWorld.java
@@ -2,6 +2,6 @@ package com.myapp;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World is running in Docker");
+        System.out.println("Hello World is running in CircleCI");
     }
 }
